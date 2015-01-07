34cab001c1f43fa3b4447853b0f9fc3077c9d693
Brian Underwood <ml+github@semi-sentient.com>
2015-01-07 16:58:44 +0200

    Works with rails, work without rails.  Can guess assocations

--- lib/neo4apis/activerecord.rb
+++ lib/neo4apis/activerecord.rb
@@ -11,7 +11,7 @@ module Neo4Apis
       self.uuid model_class.name.to_sym, model_class.primary_key
 
       self.importer model_class.name.to_sym do |object|
-        node = add_node model_class.name.to_sym, object, model_class.column_names
+        node = add_model_node model_class, object
 
         model_class.reflect_on_all_associations.each do |association_reflection|
           case association_reflection.macro
@@ -19,7 +19,7 @@ module Neo4Apis
             if options[:"import_#{association_reflection.macro}"]
               referenced_object = object.send(association_reflection.name)
               referenced_class = referenced_object.class
-              referenced_node = add_node referenced_class.name.to_sym, referenced_object, referenced_class.column_names
+              referenced_node = add_model_node referenced_class, referenced_object
 
               add_relationship association_reflection.name, node, referenced_node
             end
@@ -27,7 +27,7 @@ module Neo4Apis
             if options[:import_has_many]
               object.send(association_reflection.name).each do |referenced_object|
                 referenced_class = referenced_object.class
-                referenced_node = add_node referenced_class.name.to_sym, referenced_object, referenced_class.column_names
+                referenced_node = add_model_node referenced_class, referenced_object
 
                 add_relationship association_reflection.name, node, referenced_node
               end
@@ -37,6 +37,22 @@ module Neo4Apis
       end
 
     end
+
+    def add_model_node(model_class, object)
+      object_data = OpenStruct.new
+
+      object.attributes.each do |column, value|
+        v = if coder = model_class.serialized_attributes[column]
+              coder.dump(value)
+            else
+              value
+            end
+        object_data.send("#{column}=", v)
+      end
+
+      add_node model_class.name.to_sym, object_data, model_class.column_names
+    end
+
 
   end
 
--- lib/neo4apis/cli/activerecord.rb
+++ lib/neo4apis/cli/activerecord.rb
@@ -8,22 +8,32 @@ module Neo4Apis
     class ActiveRecord < Thor
       class_option :config_path, type: :string,  default: 'config/database.yml'
 
-      class_option :import_belongs_to, type: :boolean, default: false
-      class_option :import_has_one, type: :boolean, default: false
-      class_option :import_has_many, type: :boolean, default: false
+      class_option :import_associations, type: :boolean, default: false
+      class_option :import_belongs_to, type: :boolean, default: nil
+      class_option :import_has_one, type: :boolean, default: nil
+      class_option :import_has_many, type: :boolean, default: nil
 
+      class_option :guess_associations, type: :boolean, default: false
 
-      desc "table TABLE_NAME_OR_MODEL", "Import SQL table from ActiveRecord model"
-      def table(table_name_or_model)
+      class_option :active_record_config_path, type: :string, default: './config/database.yml'
+      class_option :active_record_environment, type: :string, default: 'development'
+
+      desc "models MODELS_OR_TABLE_NAMES", "Import SQL tables via ActiveRecord models"
+      def models(*models_or_table_names)
         setup
 
-        model_class = table_name_or_model.classify.constantize
+        puts 'models_or_table_names', models_or_table_names.inspect
+        model_classes = models_or_table_names.map(&method(:get_model))
 
-        ::Neo4Apis::ActiveRecord.model_importer(model_class)
+        model_classes.each do |model_class|
+          ::Neo4Apis::ActiveRecord.model_importer(model_class)
+        end
 
         neo4apis_client.batch do
-          model_class.find_each do |object|
-            neo4apis_client.import model_class.name.to_sym, object
+          model_classes.each do |model_class|
+            model_class.find_each do |object|
+              neo4apis_client.import model_class.name.to_sym, object
+            end
           end
         end
       end
@@ -31,16 +41,52 @@ module Neo4Apis
       private
 
       def setup
-        require './config/environment'
+        if File.exist?('config/environment.rb')
+          # Rails
+          require './config/environment' 
+        else
+          puts 'active_record_config', active_record_config.inspect
+          ::ActiveRecord::Base.establish_connection(active_record_config)
+        end
       end
 
       NEO4APIS_CLIENT_CLASS = ::Neo4Apis::ActiveRecord
 
       def neo4apis_client
         @neo4apis_client ||= NEO4APIS_CLIENT_CLASS.new(Neo4j::Session.open(:server_db, parent_options[:neo4j_url]),
-                                                       import_belongs_to: options[:import_belongs_to],
-                                                       import_has_one: options[:import_has_one],
-                                                       import_has_many: options[:import_has_many])
+                                                       import_belongs_to: import_association?(:belongs_to),
+                                                       import_has_one: import_association?(:has_one),
+                                                       import_has_many: import_association?(:has_many))
+      end
+
+      def import_association?(type)
+        options[:"import_#{type}"].nil? ? options[:import_associations] : options[:"import_#{type}"]
+      end
+
+      def get_model(model_or_table_name)
+        model_class = model_or_table_name
+        model_class = model_or_table_name.classify unless model_or_table_name.match(/^[A-Z]/)
+        model_class.constantize
+      rescue NameError
+        model_class = Object.const_set(model_class, Class.new(::ActiveRecord::Base))
+        if options[:guess_associations]
+          model_class.columns.each do |column|
+            if column.name.match(/_id$/)
+              begin
+                base = column.name.humanize.tableize.split(' ').join('_')
+
+                model_class.belongs_to base.singularize.to_sym
+              rescue NameError
+              end
+            end
+          end
+        end
+        model_class
+      end
+
+      def active_record_config
+        require 'yaml'
+        YAML.load(File.read(options[:active_record_config_path]))[options[:active_record_environment]]
       end
 
     end
